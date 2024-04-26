1b305de2e172c217306a735306b14b3e44a614eb
PYTNAG <sgds.2020.nag@yandex.ru>
2024-04-26 11:33:46 +0500

    Create project PetriImageCLI

---
+++ PetriImageCLI/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Threading.Tasks.Dataflow;
+using CLIHelper;
+using PetriImageCLI.Commands;
+
+var commands = CommandsRegisterer.RegisterCommands();
+
+Type? commandType = null;
+string[] commandArgs = [];
+
+if (args.Length == 0)
+{
+    Common.Help();
+    return;
+}
+
+if (args[0].StartsWith('-'))
+{
+    args = [
+        typeof(Common).GetCustomAttribute<CommandAttribute>()?.Name, 
+        ..args
+    ];
+}
+
+if (commands.TryGetValue(args[0], out commandType))
+{
+    commandArgs = args[1..];
+    Command command = Activator.CreateInstance(commandType) as Command ?? throw new InvalidCastException("Command type doesn't inherit Command class");
+
+    command.Parse(commandArgs);
+}
+else
+{
+    Common.Help();
+    return;
+}
