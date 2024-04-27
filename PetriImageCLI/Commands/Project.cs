99a7e80c1b1cafc3e22741e6aad6876dff095852
PYTNAG <sgds.2020.nag@yandex.ru>
2024-04-27 18:05:35 +0500

    Add subcommand new to command proj

--- PetriImageCLI/Commands/Project.cs
+++ PetriImageCLI/Commands/Project.cs
@@ -11,10 +11,27 @@ internal sealed class Project : CommandBase
     }
 
     [Command("new")]
-    private sealed class New : CommandBase
+    internal sealed class New : CommandBase
     {
+        private string _source = string.Empty;
+        private string _title = string.Empty;
+        private string _outputFolder = string.Empty;
+
+        [Flag("--source", "-s")]
+        public void SetSource(string source) => _source = source;
+
+        [Flag("--title", "-t")]
+        public void SetTitle(string title) => _title = title;
+
+        [Flag("--output", "-o")]
+        public void SetOutput(string output) => _outputFolder = output;
+
         public override void Execute()
         {
+            PetriImage.Core.Project.New(_title, _outputFolder, _source);
+
+            _finalMessage = $"Project {_title} created at {_outputFolder}";
+
             base.Execute();
         }
     }
